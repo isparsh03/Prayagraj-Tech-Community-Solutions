167446276cdb62a89a65117d2efdef9dd2a42505
devil.code_eater_isparsh_03 <166472758+isparsh03@users.noreply.github.com>
2024-05-10 23:30:52 +0530

    spiralmat.py

---
+++ spiralmat.py
@@ -0,0 +1,40 @@
+def spiral_order(matrix):
+    if not matrix:
+        return []
+    
+    result = []
+    top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
+    
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+        
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+    
+    return result
+
+# Even matrix
+even_matrix = [[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]]
+print(spiral_order(even_matrix))
+
+# Odd matrix
+odd_matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
+print(spiral_order(odd_matrix))
